397ac87a3dc472846a6e101b9c46b6b701ec15b4
songhuihui <546513056@qq.com>
2019-03-14 15:59:33 +0800

    注释

--- src/main/java/com/sh/dbsource/DataSourceAspect.java
+++ src/main/java/com/sh/dbsource/DataSourceAspect.java
@@ -1,7 +1,6 @@
 package com.sh.dbsource;
 
 import java.lang.reflect.Method;
-
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
@@ -12,23 +11,26 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 /**
- * @description: 
- * @param: 切面类
- * @return: 
- * @date: 2018-11-27 18:50
- * @auther: 季小沫的故事
+ * 切面类
+ * 2018-11-27 18:50
+ * @author 季小沫的故事
  */
-
-@Aspect //使用@Aspect声明切面
-@Component //@Component泛指组件，当组件不好归类的时候，我们可以使用这个注解进行标注。
+@Aspect
+@Component
 public class DataSourceAspect  {
 
     private static Logger LOGGER = LoggerFactory.getLogger(DataSourceAspect.class);
 
-    @Pointcut("execution(public * com.sh.service..*.*(..))") //声明切入点是service下面所有类的方法
+    /**
+     * 功能描述: 声明切入点是service下面所有类的方法
+     */
+    @Pointcut("execution(public * com.sh.service..*.*(..))")
     public void webLog(){}
 
-    @Before("webLog()") //在切入点方法执行前执行
+    /**
+     * 功能描述: 在切入点方法执行前执行
+     */
+    @Before("webLog()")
     public void before(JoinPoint point) {
 
         //获取切入点所在类的对象 UserServiceImpl
@@ -46,8 +48,10 @@ public class DataSourceAspect  {
             Method m = classz[0].getMethod(method, parameterTypes);
             //如果该方法上有DataSource的注解
             if (m != null && m.isAnnotationPresent(DataSourceChoose.class)) {
-                DataSourceChoose data = m.getAnnotation(DataSourceChoose.class);//获取注解对象
-                DynamicDataSourceHolder.putDataSource(data.value());//将注解value的值放入本地线程副本中ThreadLocal
+                //获取注解对象
+                DataSourceChoose data = m.getAnnotation(DataSourceChoose.class);
+                //将注解value的值放入本地线程副本中ThreadLocal
+                DynamicDataSourceHolder.putDataSource(data.value());
                 LOGGER.info("========获取的数据源为: {}=========", data.value());
             }
             
