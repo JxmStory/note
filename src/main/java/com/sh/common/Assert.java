2e2d73cd1cd359614aa29beceb97edae3a94f9ab
JxmStory <546513056@qq.com>
2019-05-21 15:28:26 +0800

    添加断言类Assert 入参判断封装

---
+++ src/main/java/com/sh/common/Assert.java
@@ -0,0 +1,162 @@
+package com.sh.common;
+
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.ObjectUtils;
+import org.springframework.util.StringUtils;
+
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ * @Description: 断言类(用于入参判断)
+ * @Date: 2019-5-20 15:41
+ * @Auther: 季小沫的故事
+ */
+public abstract class Assert {
+
+    public Assert() {
+
+    }
+
+    public static void isTrue(boolean expression, String message) {
+        if (!expression) {
+            throw new MyException(message);
+        }
+    }
+
+    /** @deprecated */
+    @Deprecated
+    public static void isTrue(boolean expression) {
+        isTrue(expression, "[Assertion failed] - this expression must be true");
+    }
+
+    public static void isNull(Object object, String message) {
+        if (object != null) {
+            throw new MyException(message);
+        }
+    }
+
+    /** @deprecated */
+    @Deprecated
+    public static void isNull(Object object) {
+        isNull(object, "[Assertion failed] - the object argument must be null");
+    }
+
+    public static void notNull(Object object, String message) {
+        if (object == null) {
+            throw new MyException(message);
+        }
+    }
+
+    public static void allNotNull(Object ...objects) {
+        for(int i = 0; i < objects.length; i++) {
+            if (objects[i] == null) {
+                throw new MyException("[Assertion failed] - object[" + i + "] argument must not be null");
+            }
+        }
+    }
+
+    /** @deprecated */
+    @Deprecated
+    public static void notNull(Object object) {
+        notNull(object, "[Assertion failed] - this argument is required; it must not be null");
+    }
+
+    public static void notEmpty(Object[] array, String message) {
+        if (ObjectUtils.isEmpty(array)) {
+            throw new MyException(message);
+        }
+    }
+
+    /** @deprecated */
+    @Deprecated
+    public static void notEmpty(Object[] array) {
+        notEmpty(array, "[Assertion failed] - this array must not be empty: it must contain at least 1 element");
+    }
+
+    public static void noNullElements(Object[] array, String message) {
+        if (array != null) {
+            Object[] var2 = array;
+            int var3 = array.length;
+
+            for(int var4 = 0; var4 < var3; ++var4) {
+                Object element = var2[var4];
+                if (element == null) {
+                    throw new MyException(message);
+                }
+            }
+        }
+
+    }
+
+    /** @deprecated */
+    @Deprecated
+    public static void noNullElements(Object[] array) {
+        noNullElements(array, "[Assertion failed] - this array must not contain any null elements");
+    }
+
+    public static void notEmpty(Collection<?> collection, String message) {
+        if (CollectionUtils.isEmpty(collection)) {
+            throw new MyException(message);
+        }
+    }
+
+    /** @deprecated */
+    @Deprecated
+    public static void notEmpty(Collection<?> collection) {
+        notEmpty(collection, "[Assertion failed] - this collection must not be empty: it must contain at least 1 element");
+    }
+
+    public static void notEmpty(Map<?, ?> map, String message) {
+        if (CollectionUtils.isEmpty(map)) {
+            throw new MyException(message);
+        }
+    }
+
+    /** @deprecated */
+    @Deprecated
+    public static void notEmpty(Map<?, ?> map) {
+        notEmpty(map, "[Assertion failed] - this map must not be empty; it must contain at least one entry");
+    }
+
+    public static void isInstanceOf(Class<?> type, Object obj, String message) {
+        notNull(type, "Type to check against must not be null");
+        if (!type.isInstance(obj)) {
+            instanceCheckFailed(type, obj, message);
+        }
+
+    }
+
+    public static void isInstanceOf(Class<?> type, Object obj) {
+        isInstanceOf(type, obj, "");
+    }
+
+    private static void instanceCheckFailed(Class<?> type, Object obj, String msg) {
+        String className = obj != null ? obj.getClass().getName() : "null";
+        String result = "";
+        boolean defaultMessage = true;
+        if (StringUtils.hasLength(msg)) {
+            if (endsWithSeparator(msg)) {
+                result = msg + " ";
+            } else {
+                result = messageWithTypeName(msg, className);
+                defaultMessage = false;
+            }
+        }
+
+        if (defaultMessage) {
+            result = result + "Object of class [" + className + "] must be an instance of " + type;
+        }
+
+        throw new MyException(result);
+    }
+
+
+    private static boolean endsWithSeparator(String msg) {
+        return msg.endsWith(":") || msg.endsWith(";") || msg.endsWith(",") || msg.endsWith(".");
+    }
+
+    private static String messageWithTypeName(String msg, Object typeName) {
+        return msg + (msg.endsWith(" ") ? "" : ": ") + typeName;
+    }
+}
