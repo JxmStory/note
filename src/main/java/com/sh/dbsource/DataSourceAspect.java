c4a35c78ef6c301ea7c899268f1372cdaf1712ce
JxmStory <546513056@qq.com>
2021-04-09 11:11:54 +0800

    多数据源切换

--- src/main/java/com/sh/dbsource/DataSourceAspect.java
+++ src/main/java/com/sh/dbsource/DataSourceAspect.java
@@ -1,9 +1,8 @@
 package com.sh.dbsource;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.*;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -31,8 +30,8 @@ public class DataSourceAspect  {
     /**
      * 功能描述: 在切入点方法执行前执行
      */
-//    @Around("webLog()")
-    public void around(ProceedingJoinPoint point) {
+    @Before("webLog()")
+    public void before(JoinPoint point) {
 
         MethodSignature signature = (MethodSignature) point.getSignature();
         Class<? extends Object> targetClass = point.getTarget().getClass();
@@ -42,7 +41,7 @@ public class DataSourceAspect  {
         Object target = point.getTarget();
         //获取切入点的方法名 public Result update()
         String method = point.getSignature().getName();
-        
+
         //获取切入点所在类实现的接口集合 [UserServiceInter]
         Class<?>[] classz = target.getClass().getInterfaces();
         //获取切入点方法的参数集合 [user]
@@ -59,49 +58,50 @@ public class DataSourceAspect  {
                 DynamicDataSourceHolder.putDataSource(data.value());
                 LOGGER.info("========获取的数据源为: {}=========", data.value());
             }
-            point.proceed();
         } catch (Exception e) {
             // TODO: handle exception
-        } catch (Throwable throwable) {
-            throwable.printStackTrace();
-        } finally {
-            // 手动移除该线程数据源副本
-            DynamicDataSourceHolder.removeDataSource();
         }
     }
 
-
-    /**
-     *  以下方法通过注解获取切入点
-     *  @author micomo
-     *  @date 2021/4/8 18:46
-     */
-    @Pointcut("@annotation(com.sh.dbsource.DataSourceChoose)"
-            + "|| @within(com.sh.dbsource.DataSourceChoose)")
-    public void annotationCut() {}
-
-    @Around("annotationCut()")
-    public void annotationAround(ProceedingJoinPoint point) {
-
-        // 获取类上的注解DataSourceChoose
-        Class<? extends Object> targetClass = point.getTarget().getClass();
-        DataSourceChoose targetDataSource = targetClass.getAnnotation(DataSourceChoose.class);
-        if (targetDataSource != null) {
-            // 类上没有注解时 获取方法上的注解DataSourceChoose
-            MethodSignature signature = (MethodSignature) point.getSignature();
-            Method method = signature.getMethod();
-            targetDataSource = method.getAnnotation(DataSourceChoose.class);
-        }
-        if (targetDataSource != null) {
-            DynamicDataSourceHolder.putDataSource(targetDataSource.value());
-        }
-        try {
-            point.proceed();
-        } catch (Throwable throwable) {
-            throwable.printStackTrace();
-        } finally {
-            // 销毁数据源 在执行方法之后
-            DynamicDataSourceHolder.removeDataSource();
-        }
+    @After("webLog()")
+    public void after() {
+        LOGGER.info("========移除当前线程数据源=========");
+        DynamicDataSourceHolder.removeDataSource();
     }
+
+
+//    /**
+//     *  以下方法通过注解获取切入点
+//     *  @author micomo
+//     *  @date 2021/4/8 18:46
+//     */
+//    @Pointcut("@annotation(com.sh.dbsource.DataSourceChoose)"
+//            + "|| @within(com.sh.dbsource.DataSourceChoose)")
+//    public void annotationCut() {}
+//
+//    @Around("annotationCut()")
+//    public void annotationAround(ProceedingJoinPoint point) {
+//
+//        // 获取类上的注解DataSourceChoose
+//        Class<? extends Object> targetClass = point.getTarget().getClass();
+//        DataSourceChoose targetDataSource = targetClass.getAnnotation(DataSourceChoose.class);
+//        if (targetDataSource == null) {
+//            // 类上没有注解时 获取方法上的注解DataSourceChoose
+//            MethodSignature signature = (MethodSignature) point.getSignature();
+//            Method method = signature.getMethod();
+//            targetDataSource = method.getAnnotation(DataSourceChoose.class);
+//        }
+//        if (targetDataSource != null) {
+//            LOGGER.info("========获取的数据源为: {}=========", targetDataSource.value());
+//            DynamicDataSourceHolder.putDataSource(targetDataSource.value());
+//        }
+//        try {
+//            point.proceed();
+//        } catch (Throwable throwable) {
+//            throwable.printStackTrace();
+//        } finally {
+//            // 销毁数据源 在执行方法之后
+//            DynamicDataSourceHolder.removeDataSource();
+//        }
+//    }
 }
